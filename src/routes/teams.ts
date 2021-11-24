8ffc8ffb45bbdbc9fdca65ba2790d7d9547a66c9
Mohamed Ali <mohamedali@MacBook-Pro-de-Mohamed.local>
2021-11-24 21:53:17 +0100

    Add middlewares: request params validation

--- src/routes/teams.ts
+++ src/routes/teams.ts
@@ -1,8 +1,10 @@
 import express, { Router } from "express";
 import controller from "../controllers/teams";
+import reqParamsValidation from "../middlewares/reqParamsValidation";
+
 const router: Router = express.Router();
 
 router.get("/teams", controller.getTeams);
-router.get("/teams/:id", controller.getTeam);
+router.get("/teams/:id", reqParamsValidation, controller.getTeam);
 
 export = router;
